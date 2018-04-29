d2f6f987c4feef0663b50458e146c2faa3b891bf
Bernardo Sulzbach <mafagafogigante@gmail.com>
2018-04-28 21:33:00 -0300

    Drafted Volunteer and Evaluation

---
+++ VolunteeringSystem/Models/Volunteer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VolunteeringSystem.Domain;
+
+namespace VolunteeringSystem.Models
+{
+    public class Volunteer
+    {
+        public String name
+        {
+            get;
+            set;
+        }
+        public String profession
+        {
+            get;
+            set;
+        }
+        public DateTime birthday
+        {
+            get;
+            set;
+        }
+        public String cpf
+        {
+            get;
+            set;
+        }
+        public String address
+        {
+            get;
+            set;
+        }
+        public String phone
+        {
+            get;
+            set;
+        }
+        // TODO: antecedentes criminais.
+        public List<Evaluation> evaluations
+        {
+            get;
+            set;
+        }
+    }
+}
