f2c37a2f0f233f3140c38531ebf826ab63eda6d5
Bernardo Sulzbach <bernardo@bernardosulzbach.com>
2018-06-12 13:57:13 -0300

    Got to 101% test coverage

---
+++ VolunteeringSystem.Tests/PersonCpfSetterShould.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using VolunteeringSystem.Models;
+
+namespace VolunteeringSystem.Tests
+{
+    [TestFixture]
+    public class PersonCpfSetterShould
+    {
+        [Test]
+        public void AcceptNull()
+        {
+            var unused = new Person {CPF = null};
+        }
+
+        [Test]
+        public void NotAcceptEmptyString()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var unused = new Person {CPF = ""};
+            });
+        }
+
+        [Test]
+        public void NotAcceptInvalidStrings()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var unused = new Person {CPF = "Oi"};
+            });
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var unused = new Person {CPF = "10"};
+            });
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var unused = new Person {CPF = "546.760.405-85"};
+            });
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var unused = new Person {CPF = "546.760.405-50"};
+            });
+        }
+
+        [Test]
+        public void AcceptValidStrings()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var unused = new Person {CPF = "54676040580"};
+            });
+            Assert.DoesNotThrow(() =>
+            {
+                var unused = new Person {CPF = "546760405-80"};
+            });
+            Assert.DoesNotThrow(() =>
+            {
+                var unused = new Person {CPF = "546.760.405-80"};
+            });
+        }
+    }
+}
--- VolunteeringSystem/Models/Person.cs
+++ VolunteeringSystem/Models/Person.cs
@@ -13,7 +13,39 @@ namespace VolunteeringSystem.Models
 
         public DateTime birthDate { get; set; }
 
-        public string CPF { get; set; }
+        private static bool validate_cpf(string cpf)
+        {
+            if (cpf == null) return true;
+            Console.WriteLine("Hey.");
+            cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11) return false;
+            var helper = cpf.Substring(0, 9);
+            var sum = 0;
+            for (var i = 0; i < 9; i++) sum += int.Parse(helper[i].ToString()) * (10 - i);
+            var remainder = sum % 11;
+            if (remainder < 2) remainder = 0;
+            else remainder = 11 - remainder;
+            var digit = remainder.ToString();
+            helper = cpf.Substring(0, 9) + digit;
+            sum = 0;
+            for (var i = 0; i < 10; i++) sum += int.Parse(helper[i].ToString()) * (11 - i);
+            remainder = sum % 11;
+            if (remainder < 2) remainder = 0;
+            else remainder = 11 - remainder;
+            return cpf.EndsWith(digit + remainder);
+        }
+
+        private string _cpf;
+
+        public string CPF
+        {
+            get => _cpf;
+            set
+            {
+                if (!validate_cpf(value)) throw new ArgumentOutOfRangeException();
+                _cpf = value;
+            }
+        }
 
         public Sex sex { get; set; }
 
